37cd0192abb2ee0fbef8ccf762c2623c654c949d
RyotaroKAMOI <r.kamoi@scanspecial.jp>
2025-07-21 13:55:27 +0900

    initial commit

---
+++ src/main/java/com/snackboy/tabico/TabicoApplication.java
@@ -0,0 +1,13 @@
+package com.snackboy.tabico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snackboy/tabico/TabicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snackboy.tabico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
